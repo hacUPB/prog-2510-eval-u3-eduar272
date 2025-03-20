67a51884e2c1eaa0c62cf8dc7281c275809fbd3e
eduardo <eduarries15@gmail.com>
2025-03-20 15:36:26 -0500

    20/03/25

---
+++ UNIDAD 3/README.md
@@ -0,0 +1,6 @@
+# Unidad No. 
+## Información del estudiante  
+Nombre del estudiante: Eduardo Arrieta Espitia
+Id.:000543069
+---
+
---
+++ UNIDAD 3/bucles/ejercicio_4.py
@@ -0,0 +1,14 @@
+numeros_pares = []
+numero = int(input("¿cuántos pares quiere guardar? "))
+
+#for i in range(numero):
+#    numeros_pares.append(i*2)
+
+#cont = 0
+#for i in range(numero):
+#   cont += 2
+#for i in range(numero):
+#    numeros_pares.append()
+
+print(numeros_pares)
+
